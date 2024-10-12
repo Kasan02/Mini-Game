0cf061706f00c7726a04759309f5b21987971844
Kasan02 <kasanovd0202@gmail.com>
2024-10-12 22:16:36 +0300

    Отредактировал index и mobile, создал script.js и поработал над заданиями

---
+++ script.js
@@ -0,0 +1,61 @@
+// Задание 1
+let product = Number(alert('10'));
+let productPrice = Number(alert('20'));
+// Задание 2
+const iphone = Number(alert('2007'));
+// Задание 3
+const name = Text(alert('Брендан'));
+// Задание 4
+let one = 10;
+let two = 2;
+let tree = 9;
+let addition = alert(one + two);
+let subtraction = alert(one - two);
+let multiplication = alert(one * two);
+let division = alert(one / two);
+// Задание 5
+alert(two ** 5);
+// Задание 6
+alert(tree % two);
+// Задание 7
+let num = 1;
+alert(num += 5);
+alert(num -= 3);
+alert(num *= 7);
+alert(num /= 3);
+alert(num++);
+alert(num--);
+alert(num);
+// Задание 8
+let age = Number(prompt("Сколько вам лет?"));
+let result = alert(age);
+// Задание 9
+let user = {
+    name: 'Max',
+    age: '28',
+    isAdmin: 'Max02'
+}
+// Задание 10
+let names = prompt("Сколько вам лет?");
+alert(age);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
