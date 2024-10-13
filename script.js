0d2acad4ca0f81e5c86248d78bee8362adda0996
Kasan02 <kasanovd0202@gmail.com>
2024-10-13 21:53:19 +0300

    Написал задния по скрипту(Не редактировал index.html)

--- script.js
+++ script.js
@@ -1,43 +1,119 @@
 // Задание 1
-let product = Number(alert('10'));
-let productPrice = Number(alert('20'));
+// let product = Number(alert('10'));
+// let productPrice = Number(alert('20'));
 // Задание 2
-const iphone = Number(alert('2007'));
+// const iphone = Number(alert('2007'));
 // Задание 3
-const name = (alert('Брендан'));
+// const name = (alert('Брендан'));
 // Задание 4
-let one = 10;
-let two = 2;
-let tree = 9;
-let addition = alert(one + two);
-let subtraction = alert(one - two);
-let multiplication = alert(one * two);
-let division = alert(one / two);
+// let one = 10;
+// let two = 2;
+// let tree = 9;
+// let addition = alert(one + two);
+// let subtraction = alert(one - two);
+// let multiplication = alert(one * two);
+// let division = alert(one / two);
 // Задание 5
-alert(two ** 5);
+// alert(two ** 5);
 // Задание 6
-alert(tree % two);
+// alert(tree % two);
 // Задание 7
-let num = 1;
-alert(num += 5);
-alert(num -= 3);
-alert(num *= 7);
-alert(num /= 3);
-alert(num++);
-alert(num--);
-alert(num);
+// let num = 1;
+// alert(num += 5);
+// alert(num -= 3);
+// alert(num *= 7);
+// alert(num /= 3);
+// alert(num++);
+// alert(num--);
+// alert(num);
 // Задание 8
-let age = Number(prompt("Сколько вам лет?"));
-let result = alert(age);
+// let age = Number(prompt("Сколько вам лет?"));
+// let result = alert(age);
 // Задание 9
-let user = {
-    name: 'Max',
-    age: 28, 
-    isAdmin: Max02 = true,
-}
+// let user = {
+//     name: 'Max',
+//     age: 28, 
+//     isAdmin: Max02 = true,
+// }
 // Задание 10
-let names = prompt("Как вас зовут?");
-alert(`Привет, ${names}!`);
+// let names = prompt("Как вас зовут?");
+// alert(`Привет, ${names}!`);
+
+// Hw3
+// Задание 1
+// let password = String(prompt('Введите пароль'))
+// if (password === '0202') {
+//     console.log('Пароль введён верно');
+// } else {
+//     console.log('Пароль введён неправильно');
+// }
+// Задание 2
+// let c = 2;
+// if (c > 0 && c < 10) {
+//     console.log('Верно');
+// } else {
+//     console.log('Неверно');
+// }
+// Задание 3
+// let d = 60;
+// let e = 50;
+// if (d || e > 100) {
+//     console.log('Верно');
+// } else {
+//     console.log('Неверно');
+// }
+// Задание 4
+// let f = '2';
+// let g = '3';
+// alert(f + g);
+// Задание 5
+let number = Number(prompt('Введите номер месяца'));
+
+switch (monthNumber = 12) {
+    case 1:
+        console.log('Зима');
+        break;
+    case 2:
+        console.log('Зима');
+        break;
+    case 3:
+        console.log('Зима');
+        break;
+    case 4:
+        console.log('Весна');
+        break;
+    case 5:
+        console.log('Весна');
+        break;
+    case 6:
+        console.log('Весна');
+        break;
+    case 7:
+        console.log('Лето');
+        break;
+    case 8:
+        console.log('Лето');
+        break;
+    case 9:
+        console.log('Лето');
+        break;
+    case 10:
+        console.log('Осень');
+        break;
+    case 11:
+        console.log('Осень');
+        break;
+    case 12:
+        console.log('Осень');
+        break;
+    default:
+        console.log('Ошибка!')
+}
+
+
+
+
+
 
 
 
