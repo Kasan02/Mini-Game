709d9f91cbb89e11959408b0482b4c35510f8780
Kasan02 <kasanovd0202@gmail.com>
2024-10-14 19:55:16 +0300

    Убрал ссылки в 3 блоке, под редактировал script.js, создал tasks.html для скрипта

--- script.js
+++ script.js
@@ -1,43 +1,43 @@
 // Задание 1
-let product = Number(alert('10'));
-let productPrice = Number(alert('20'));
+// let product = Number(alert('10'));
+// let productPrice = Number(alert('20'));
 // Задание 2
-const iphone = Number(alert('2007'));
+// const iphone = Number(alert('2007'));
 // Задание 3
-const name = (alert('Брендан'));
+// const name = (alert('Брендан'));
 // Задание 4
-let one = 10;
-let two = 2;
-let tree = 9;
-let addition = alert(one + two);
-let subtraction = alert(one - two);
-let multiplication = alert(one * two);
-let division = alert(one / two);
+// let one = 10;
+// let two = 2;
+// let tree = 9;
+// let addition = alert(one + two);
+// let subtraction = alert(one - two);
+// let multiplication = alert(one * two);
+// let division = alert(one / two);
 // Задание 5
-alert(two ** 5);
+// alert(two ** 5);
 // Задание 6
-alert(tree % two);
+// alert(tree % two);
 // Задание 7
-let num = 1;
-alert(num += 5);
-alert(num -= 3);
-alert(num *= 7);
-alert(num /= 3);
-alert(num++);
-alert(num--);
-alert(num);
+// let num = 1;
+// alert(num += 5);
+// alert(num -= 3);
+// alert(num *= 7);
+// alert(num /= 3);
+// alert(num++);
+// alert(num--);
+// alert(num);
 // Задание 8
-let age = Number(prompt("Сколько вам лет?"));
-let result = alert(age);
+// let age = Number(prompt("Сколько вам лет?"));
+// let result = alert(age);
 // Задание 9
-let user = {
-    name: 'Max',
-    age: 28, 
-    isAdmin: Max02 = true,
-}
+// let user = {
+//     name: 'Max',
+//     age: 28, 
+//     isAdmin: true,
+// }
 // Задание 10
-let names = prompt("Как вас зовут?");
-alert(`Привет, ${names}!`);
+// let names = prompt("Как вас зовут?");
+// alert(`Привет, ${names}!`);
 
 
 
