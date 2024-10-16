b99568dc12420ce0f9ca4315a306383810838dca
Kasan02 <kasanovd0202@gmail.com>
2024-10-16 21:26:23 +0300

    Подготовил к сдаче

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 // Hw2
+
 // Задание 1
 // let product = Number(alert('10'));
 // let productPrice = Number(alert('20'));
@@ -41,6 +42,7 @@
 // alert(`Привет, ${names}!`);
 
 // Hw3
+
 // Задание 1
 // let password = prompt('Введите пароль');
 // if (password === '0202') {
@@ -68,32 +70,97 @@
 // let g = '3';
 // alert(Number(f) + Number(g));
 // Задание 5
-let monthNumber = Number(prompt('Введите номер месяца'));
+// let monthNumber = Number(prompt('Введите номер месяца'));
 
-switch (monthNumber = 12) {
-    case 12:
-    case 1:
-    case 2:
-        console.log('Зима');
-    break;
-    case 3:
-    case 4:
-    case 5:
-        console.log('Весна');
-    break;
-    case 6:
-    case 7:
-    case 8:
-        console.log('Лето');
-    break;
-    case 9:
-    case 10:
-    case 11:
-        console.log('Осень');
-    break;
-    default:
-        console.log('Ошибка!')
-}
+// switch (monthNumber) {
+//     case 12:
+//     case 1:
+//     case 2:
+//         console.log('Зима');
+//     break;
+//     case 3:
+//     case 4:
+//     case 5:
+//         console.log('Весна');
+//     break;
+//     case 6:
+//     case 7:
+//     case 8:
+//         console.log('Лето');
+//     break;
+//     case 9:
+//     case 10:
+//     case 11:
+//         console.log('Осень');
+//     break;
+//     default:
+//         console.log('Ошибка!')
+// }
+
+// Hw4
+
+// Задание 1
+// let w = 0;
+
+// while (w < 2) {
+//     console.log("Привет");
+//     w++;
+// }
+// Задание 2
+// let s = 1;
+
+// while (s <= 5) {
+//     console.log(s)
+//     s++;
+// }
+// Задание 3
+// let b = 7;
+
+// while (b <= 22) {
+//     console.log(b)
+//     b++
+// }
+// Задание 4
+// const obj = {
+//     Коля: '200',
+//     Вася: '300',
+//     Петя: '400',
+// }
+
+// for (const key in obj) {
+//     console.log(`${key} - ${obj[key]}$`);
+// }
+
+// Задание 5
+
+// let k = 1000;
+// let num = 0;
+
+// while (k >= 50) {
+//     k /= 2;
+//     console.log(k)
+//     num++;
+// }
+// console.log(`Количество циклов: ${num}`);
+
+// Задание 6
+
+// let dayFriday = 2;
+// let allDay = 31;
+
+// for (let i = dayFriday; i <= allDay; i += 7) {
+//     console.log(`Сегодня пятница, ${i}-е число. Необходимо подготовить отчет.`);   
+// }
+
+
+
+
+
+
+
+
+
+
 
 
 
