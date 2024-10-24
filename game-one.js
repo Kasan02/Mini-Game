a4c6bed07193133563ab13104489b5d886ea6c5f
Kasan02 <kasanovd0202@gmail.com>
2024-10-24 21:50:10 +0300

    Написал игру через else if

--- game-one.js
+++ game-one.js
@@ -1,20 +1,18 @@
-// let question = Number(prompt('Введите первое число'));
+const question = Number(prompt('Угадайте число от 1 до 100'));
+let randomInt = 40;
 
-// function questionGame(question) {
-//     if ()
-// }
-function starGame() {  
-    const randomInt = getRandomInt(0,100);
-    console.log(randomInt)
-    let question = Number(prompt('Угадай число от 1 до 100'));
-  
+function gameOne(question) {  
     if (isNaN(question)) {
-      alert("Введено не число")
+      return('Введено не число, повторите снова.');
     } else if (question === randomInt) {
-      alert('Угадал'); 
-    } else if (randomInt <= 50) {
-      alert ('Это число меньше 51');
+      return ('Ура, победа!!!Ждем тебя снова в нашей игре.'); 
+    } else if (question <= 50 && question >= 41) {
+      return ('Это число меньше 41 и уже близко, тихо, а то спугнём.');
+    } else if (question >= 30 && question <= 39) {
+      return ('Это число больше 39 и уже где-то рядом, чувствуете?');
     } else {
-      alert ('это число больше 50');
-    } 
+      return ('Вы не угадали, попробуйте еще.');
+    }
   };
+  alert(gameOne(question));
+  
