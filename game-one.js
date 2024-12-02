a7762d39fe03755bf577cd34416285767f1bf1cf
Kasan02 <kasanovd0202@gmail.com>
2024-12-02 20:05:14 +0300

    Запутался

--- game-one.js
+++ game-one.js
@@ -21,7 +21,6 @@ do {
 
 const buttonGameTwo = document.getElementById("twoGame");
 buttonGameTwo.addEventListener("click", () => {
-function simpleMath() {
   function getRandomInt(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
@@ -33,9 +32,19 @@ function simpleMath() {
   const signs = [`+`, `-`, `*`, `/`];
   const randomIndex = Math.floor(Math.random() * (signs.length));
   let randomSigns = signs[randomIndex];
-  function getResult() {
     let userAnswear = Number(prompt(`Решите задачу? \n ${randomInt1} ${randomSigns} ${randomInt2}`));
-    let result;
+    let result = randomSigns
+
+    switch(randomSigns) {
+      case "+": result = randomInt1 + randomInt2; 
+      break;
+      case "-": result = randomInt1 - randomInt2;  
+      break;
+      case "*": result = randomInt1 * randomInt2;  
+      break;
+      case "/": result = Math.floor(randomInt1 / randomInt2);  
+      break;
+    }
 
     if (userAnswear === result) {
       alert(`Вы решили задачу Верно`);
@@ -43,8 +52,5 @@ function simpleMath() {
       alert(`Вы решили задачу Неверно`)
     }
   }
+)
   return(getResult());
-}
-alert(simpleMath());
-
-});
