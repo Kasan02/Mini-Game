2c25be250df47d0db081adc0fca80346106604e8
Kasan02 <kasanovd0202@gmail.com>
2024-12-03 21:28:07 +0300

    Мини-правка

--- game-tree.js
+++ game-tree.js
@@ -1,15 +1,12 @@
 const buttonGameTree = document.getElementById("treeGame");
 buttonGameTree.addEventListener("click", () => {
 
-function reverseString(joinArray) {    
+function reverseString() {    
     let splitString = String(prompt("Введите ваш текст")).split();  
     let reverseArray = splitString.reverse(); 
     let joinArray = reverseArray.join(""); 
-         // "olleh" 
 
     return joinArray; 
 } 
-
-alert(joinArray); 
-
 });
+alert (joinArray);
