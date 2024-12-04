869fef658f6878ed186dd879f485eeee82441191
Kasan02 <kasanovd0202@gmail.com>
2024-12-04 18:47:27 +0300

    Сделал связь с игрой

--- game-one.js
+++ game-one.js
@@ -3,16 +3,16 @@
 const buttonGameOne = document.getElementById("oneGame");
 buttonGameOne.addEventListener("click", () => {
   const a = Math.floor(Math.random() * 100) + 1;
-let num = 0;
+  let num = 0;
 
-do {
-  num = Number(prompt('Отгадай число от 1 до 100'));
-  if (num === a) {
-    alert('Угадал!');
-  } else if (num > a) {
-    alert('Твой ответ больше загаданного числа. Попробуй ещё!');
-  } else if (num < a) {
-    alert('Твой ответ меньше загаданного числа. Попробуй ещё!');
-  }
-} while (num !== a);
+  do {
+    num = Number(prompt('Отгадай число от 1 до 100'));
+    if (num === a) {
+      alert('Угадал!');
+    } else if (num > a) {
+      alert('Твой ответ больше загаданного числа. Попробуй ещё!');
+    } else if (num < a) {
+      alert('Твой ответ меньше загаданного числа. Попробуй ещё!');
+    }
+  } while (num !== a);
 });
