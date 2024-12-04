6fb53545fce56a9ffed2102d93c65c529feefb7e
Kasan02 <kasanovd0202@gmail.com>
2024-12-04 19:23:03 +0300

    Переделал игру "Переверни текст"

--- game-tree.js
+++ game-tree.js
@@ -1,12 +1,21 @@
+// 3 игра
+
 const buttonGameTree = document.getElementById("treeGame");
 buttonGameTree.addEventListener("click", () => {
 
-function reverseString() {    
-    let splitString = String(prompt("Введите ваш текст")).split();  
-    let reverseArray = splitString.reverse(); 
-    let joinArray = reverseArray.join(""); 
+    function reverseText() {
+        let text = prompt("Введите текст, а мы его переверенем");
+        
+        text = text.split(" ");
+        
+        let res = [];
+        
+        for (let i = 0; i < text.length; i++){
+        res.push (text[i].split("").reverse().join(""));
+        }
+        
+        return alert(`Вот что получилось: ${res.join("")}`);
+        }
+        reverseText();
+    });
 
-    return joinArray; 
-} 
-});
-alert (joinArray);
