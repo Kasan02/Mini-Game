089be438cb4cd08e20e8d55f4c0fcdef45322cf5
Kasan02 <kasanovd0202@gmail.com>
2024-12-05 20:26:38 +0300

    1 задание

--- script.js
+++ script.js
@@ -482,5 +482,15 @@
 // }
 // console.log(formatDate(new Date()));
 
+// HW8
 
-ввпаввыывыв
+// Задание 1
+
+const people = [
+    { name: 'Глеб', age: 29 },
+    { name: 'Анна', age: 17 },
+    { name: 'Олег', age: 7 },
+    { name: 'Оксана', age: 47 }
+ ];
+
+ console.log(people.sort((a, b) => b.age - a.age));
