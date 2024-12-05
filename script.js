2e7553219988c408162f49c64309fe8c668c2ae9
Kasan02 <kasanovd0202@gmail.com>
2024-12-05 21:02:03 +0300

    Написал задания

--- script.js
+++ script.js
@@ -494,6 +494,70 @@ const people = [
     { name: 'Оксана', age: 47 }
  ];
 
- console.log(people.sort((a, b) => b.age - a.age));
+ console.log(people.sort((a, b) => a.age - b.age));
 
+// Задание 2
 
+function filter(array, ruleFunction) {
+    const result = [];
+    for (let i = 0; i < array.length; i++) {
+        if (ruleFunction(array[i])) {
+            result.push(array[i]);
+        }
+    }
+    return result;
+}
+
+function isPositive(num) {
+    return num > 0;
+    }
+    function isMale(person) {
+    return person.gender === 'male'
+    }
+    
+    console.log(filter([5, -4, 1, 9], isPositive));
+    
+    const people = [
+       {name: 'Глеб', gender: 'male'},
+       {name: 'Анна', gender: 'female'},
+       {name: 'Олег', gender: 'male'},
+       {name: 'Оксана', gender: 'female'}
+    ];
+    
+    console.log(filter(people, isMale));
+
+    // Задание 3
+
+    const intervalId = setInterval(() => {
+        console.log(new Date());
+    }, 10000);
+
+    setTimeout(() => {
+        clearInterval(intervalId);
+        console.log('30 секунд прошло');
+        }, 30000);
+
+        // Задание 4
+
+        function delayForSecond(callback) {
+            setTimeout(callback, 1000);
+        }
+        
+        delayForSecond(function () {
+           console.log('Привет, Глеб!');
+        });
+
+        // Задание 5
+
+function delayForSecond(cb) {
+    setTimeout(() => {
+        console.log('Прошла одна секунда');
+        if(cb) {  cb(); }
+    }, 1000)
+}
+
+function sayHi (name) {
+    console.log(`Привет, ${name}!`);
+}
+
+delayForSecond(() => sayHi('Глеб'));
