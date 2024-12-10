c8c54a110c1d5ce31f3380addf62013e6ad9f932
Kasan02 <kasanovd0202@gmail.com>
2024-12-10 19:47:24 +0300

    Написал 4 задания

--- hw 9 задания.js
+++ hw 9 задания.js
@@ -1,6 +1,6 @@
 // <!-- Задание 1 -->
 const btnEl = document.querySelector(".btn-hide");
-const titleEl = document.querySelector(".title");
+const titleEl = document.querySelector(".title-hide");
 
 console.log(titleEl);
 console.log(btnEl);
@@ -21,3 +21,50 @@ btnEl.addEventListener("click", () => {
 }
 btnClick()
 });
+
+// <!-- Задание 2 -->
+const btnElColor = document.querySelector(".btn-color");
+const textElColor = document.querySelector(".text-color");
+
+console.log(textElColor);
+console.log(btnElColor);
+
+btnElColor.addEventListener("click", () => {
+    
+    function btnColorClick() {
+        textElColor.style.color = textElColor.style.color === "blue"? "black" : "blue";
+}
+btnColorClick()
+});
+
+// <!-- Задание 3 -->
+
+const btnElChange = document.querySelector(".btn-change");
+const titleElChange = document.querySelector(".title-change");
+
+console.log(titleElChange);
+console.log(btnElChange);
+
+btnElChange.addEventListener("click", () => {
+
+    function btnChangeClick() {
+
+        titleElChange.textContent = titleElChange.textContent === "Пока, мир!"? "Привет, мир!" : "Пока, мир!";
+}
+btnChangeClick()
+});
+
+// <!-- Задание 4 -->
+
+    const descriptionEl = document.querySelectorAll(".description");
+
+    console.log(descriptionEl);
+    descriptionEl.forEach(description => {
+
+        description.textContent = "Измененное описание";
+    });
+
+
+
+
+
